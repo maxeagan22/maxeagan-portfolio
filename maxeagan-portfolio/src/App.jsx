21bf08075175bacef97271633fae733a992a8a1c
Max Eagan <meagan7654@gmail.com>
2025-07-08 16:01:11 -0500

    Add mobile menu

--- maxeagan-portfolio/src/App.jsx
+++ maxeagan-portfolio/src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import "./index.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
@@ -13,7 +16,9 @@ function App() {
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
-        <Navbar />
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
     </>
   );
