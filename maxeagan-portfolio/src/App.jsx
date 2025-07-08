3cb40949f5cb9067abde462e370e53f1570238de
Max Eagan <meagan7654@gmail.com>
2025-07-08 15:11:18 -0500

    add loading screen

--- maxeagan-portfolio/src/App.jsx
+++ maxeagan-portfolio/src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen";
 
 function App() {
-  return <></>;
+  const [isLoaded, setIsLoaded] = useState(false);
+  return <>{!isLoaded && <LoadingScreen />}</>;
 }
 
 export default App;
